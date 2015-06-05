f3f0a38da745c0ca048734ccc5cbdad87d7cb3cf
Stephen Van Zee <svanzee@caringbridge.org>
2015-06-05 08:23:42 -0500

    changing depenencies

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,12 @@ description      'Installs/Configures role-BI-apache-phalcon'
 long_description 'Installs/Configures role-BI-apache-phalcon'
 version          '0.1.1'
 
-depends 'httpd', '~> 0.2.6'
-depends 'php', '~> 1.5.0'
-depends 'yum-remi', '~> 0.1.0'
-
-depends 'zendserver', '~>1.1.1'
+# depends 'httpd', '~> 0.2.6'
+# depends 'php', '~> 1.5.0'
+# depends 'yum-remi', '~> 0.1.0'
+#
+# depends 'zendserver', '~>1.1.1'
 depends 'role-zendserver'
+depends 'yum'
+depends 'zendserver', '~>1.1.1'
+depends 'yum-epel'
